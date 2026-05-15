58191b045a0780bdf84cd2776501f8b26e3a94c0
shahidsss8998-debug <shahidsss8998@gmail.com>
2026-05-15 20:25:19 +0530

    Bulletproof Regex catch-all for Express 5

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use('/api/portal', portalRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('(.*)', (req, res) => {
+  app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
 } else {
